e00da83e576afa470fafdeb7682b1d4d0a2966c6
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2021-08-21 19:56:51 +0800

    Remove unnecessary print statements.

--- Intermediate_Decryption.py
+++ Intermediate_Decryption.py
@@ -24,19 +24,19 @@ for i in raw_text:
 
 # This just tells us how long is the original unencrypted raw text!
 Num_Digit_length = int(list_1[1])
-print(f"Number of Digit for Length of Text: {Num_Digit_length}")
+# print(f"Number of Digit for Length of Text: {Num_Digit_length}")
 
 for ii in range(3, Num_Digit_length + 3):
     length_raw_text += str(list_1[ii])
 
-print(f"Length of Text: {length_raw_text}")
+# print(f"Length of Text: {length_raw_text}")
 
 # This will remove the numbers that is used to describe on how long is the original unencrypted raw text.
 for iii in range(0, Num_Digit_length + 3):
     del list_1[0]
 
-print(f"Length of List: {len(list_1)}")
-print(list_1)
+# print(f"Length of List: {len(list_1)}")
+# print(list_1)
 
 list_1_len = len(list_1)
 
@@ -54,27 +54,27 @@ for iii in range(-1, -(list_1_len), -1):
 # After reading the characters from right to left, it will need to be inverted laterally back to normal...
 noise_text_2 = noise_text[::-1]
 
-print(f"Length of Noise: {len(noise_text_2)}")
-print(noise_text_2)
+# print(f"Length of Noise: {len(noise_text_2)}")
+# print(noise_text_2)
 
 # Then we remove all the letters of the list_1 until we are only left with numbers.
 for iiii in range(0, len(noise_text_2)):
     list_1.pop()
 
-print(f"Length of List: {list_1_len}")
-print(list_1)
+# print(f"Length of List: {list_1_len}")
+# print(list_1)
 
 # Then convert that list_1 into a string of numbers without any commas or so.
 for iv in list_1:
     new_text += str(iv)
 
-print(new_text)
+# print(new_text)
 
 # Convert the letters we gathered before into its ASCII equivalents.
 for vi in noise_text_2:
     new_digi += str(ord(vi))
 
-print(new_digi)
+# print(new_digi)
 
 # That number we obtained from the ASCII equivalents of those noise text will be added to the new_text.
 new_digi_1 = int(new_text) + int(new_digi)
@@ -83,35 +83,35 @@ new_digi_1 = int(new_text) + int(new_digi)
 if int(new_text[0:1]) == 0:
     new_digi_1 = "0" + str(new_digi_1)
 
-print(new_digi_1)
+# print(new_digi_1)
 
 # This checks how many swap digits there are by seeing if it is a multiple of 3 under a certain condition!
 length_digits = len(str(new_digi_1))
 
 if length_digits % 3 == 0:
     num_swap_digit = 3
-    print("3 swap digit")
+    # print("3 swap digit")
 elif (length_digits - 2) % 3 == 0:
     num_swap_digit = 2
-    print("2 swap digit")
+    # print("2 swap digit")
 else:
     num_swap_digit = 1
-    print("1 swap digit")
+    # print("1 swap digit")
 
-print(num_swap_digit)
+# print(num_swap_digit)
 
 # This converts new_digi_1 into a list.
 for vtr in str(new_digi_1):
     list_2.append(vtr)
 
-print(list_2)
+# print(list_2)
 
 # This will store the the swap index in the form of a list!
 for vvi in range(-1, -(num_swap_digit) - 1, -1):
     nig = list_2[vvi]
     swap_list.append(nig)
 
-print(swap_list)
+# print(swap_list)
 
 # Only if the length of original raw text is more than or equal to 4 characters long then will we do the swapping...
 if int(length_raw_text) >= 4:
@@ -123,13 +123,13 @@ if int(length_raw_text) >= 4:
 else:   # If no swapping is required, then this executes.
     print("Too Short!")
 
-print(list_2)
+# print(list_2)
 
 # This removes the swap index from the list_2... Which is at the rightmost side of the list. So we can use .pop() method!
 for vvvv in range(0, num_swap_digit):
     list_2.pop()
 
-print(list_2)
+# print(list_2)
 
 # This generates a list of 3 digits from list_2...
 for vvti in list_2:
@@ -144,7 +144,7 @@ for vvti in list_2:
 
 list_3.append(new_text_1)
 
-print(list_3)
+# print(list_3)
 
 # If the length of original raw text is less than 4, no further action is required... Just minus 23 and divide by 7...
 if int(length_raw_text) < 4:
@@ -163,8 +163,8 @@ else:
 
     if midswap == 1:    # This is only applicable for the case where the original raw text is exactly 4 characters long...
         midswap += 1
-    else:
-        print("I'm a Failure!")
+    # else:
+        # print("I'm a Failure!")
 
     ax = list_3[1]
     bx = list_3[midswap]
@@ -182,7 +182,7 @@ else:
     for iix in list_3:
         list_4.append(int((int(iix) - 23) / 7))
 
-    print(list_4)
+    # print(list_4)
 
     # Convert back to ASCII characters.
     for iy in list_4:
--- Intermediate_Encryption.py
+++ Intermediate_Encryption.py
@@ -32,7 +32,7 @@ for i in raw_text:
     conversion_2 = (conversion_2 * 7) + 23
     final_muladd.append(conversion_2)
 
-print(final_muladd)
+# print(final_muladd)
 
 # This part ensures that each characters will have 3 digits no matter what.
 for ii in final_muladd:
@@ -46,7 +46,7 @@ for ii in final_muladd:
     else:
         final_3digi.append(conversion_4)
 
-print(final_3digi)
+# print(final_3digi)
 
 # Swap the first and the last character (remember that each character takes up 3 numbers which is in the form of encrypted ASCII), and then we will take the [(length of string) - 2] and divide by 2 in the form of integer. That number obtained will be the character that will be swapped with the second character of the list...
 if len(final_3digi) >= 4:
@@ -62,7 +62,7 @@ if len(final_3digi) >= 4:
     dd = final_3digi[length_swap]
     final_3digi[length_swap] = cc
     final_3digi[1] = dd
-    print(final_3digi)
+    # print(final_3digi)
 
 else:
     print("Please enter longer text!")  # This will be printed when there isn't enough characters in the raw text to perform the full encryption...
@@ -74,8 +74,8 @@ for iii in final_3digi:
 for iic in strlistRMV:
     final_listRMV.append(int(iic))
 
-print(strlistRMV)
-print(final_listRMV)
+# print(strlistRMV)
+# print(final_listRMV)
 
 # Traverse through the entire string and look for 3 distinct numbers, and we need at most 3 digits and then use that 3 digits each as an index to the list of encrypted ASCII say the 3 digits are 2, 3, 4 then we will swap index 2 with 3, index 3 with 4 and index 4 with 5 sequentially...
 for u1 in final_listRMV:
@@ -100,7 +100,7 @@ for u1 in final_listRMV:
             abc122.append(u1)
             count += 1
 
-print(abc122)
+# print(abc122)
 
 if len(raw_text) >= 4:  # We have to ensure that the length of the final_listRMV is at least 11 because the highest index is 9 which will trigger out of range at line 112 (9 + 1) when the list is insufficiently long.
     if len(abc122) >= 3:    # This checks the number of swap index.
@@ -123,7 +123,7 @@ if len(raw_text) >= 4:  # We have to ensure that the length of the final_listRMV
         final_listRMV[key3 + 1] = at
         final_listRMV[key3] = at1
 
-        print(final_listRMV)
+        # print(final_listRMV)
 
     elif len(abc122) == 2:  # This checks the number of swap index.
         key1 = abc122[0]
@@ -139,7 +139,7 @@ if len(raw_text) >= 4:  # We have to ensure that the length of the final_listRMV
         final_listRMV[key2 + 1] = at
         final_listRMV[key2] = at1
 
-        print(strlistRMV)
+        # print(strlistRMV)
 
     elif len(abc122) == 1:  # This checks the number of swap index.
         key1 = abc122[0]
@@ -160,7 +160,7 @@ else:   # If there isn't at least 4 characters in the raw text, this will be tri
 # This is the hint to the 3 distinct number index which we do the swapping and to the original number of characters in the raw text. The first number between the 2 '@' indicates the number of digit of the number of characters in the raw string, so the following numbers after the second '@' are the total number of digit. Then the last 3 character is the index which the swapping occurs and that the index is zero at the number indicating the number of characters in the raw text!
 final_listRMV_2 = lent + final_listRMV + abc122
 
-print(final_listRMV_2)
+# print(final_listRMV_2)
 
 # This is to do a negative indexing to sequentially get 2 digit numbers with a step of -1 and then take these 2 digit numbers and convert to their ASCII equivalent but however it cannot be numbers neither can it be '@'.
 for gghh in range(-4, -len(final_listRMV_2) + 2, -1):
@@ -184,7 +184,7 @@ for gghh in range(-4, -len(final_listRMV_2) + 2, -1):
         iterstring += str(final_listRMV_2[gghh])
         count3 += 1
 
-print(final_listRMV_3)
+# print(final_listRMV_3)
 
 # Taking the encrypted number starting from 4th index to the last before we add the ASCII noise...
 final_listRMV_4 = final_listRMV_2
@@ -194,16 +194,16 @@ for iif in range(0, 4):
 for jjh in final_listRMV_4:
     strlistRMV2 += str(jjh)
 
-print(crap)
-print(strlistRMV2)
+# print(crap)
+# print(strlistRMV2)
 
 # Combining the index of ASCII noise into a number which we will subtract from the strlistRMV2...
 for utt in final_listRMV_3:
     concat_noise += str(ord(utt))
 
-print(concat_noise)
+# print(concat_noise)
 newstrlistRMV2 = int(strlistRMV2) - int(concat_noise)
-print(newstrlistRMV2)
+# print(newstrlistRMV2)
 
 # Combining the abomination!
 finally222 += crap
@@ -217,7 +217,7 @@ for itit in FINALE_1:
 
 finally222 += final_listRMV_3
 
-print(finally222)
+# print(finally222)
 
 for nga in finally222:
     finalMAN += str(nga)
